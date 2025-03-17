217485061dc37ae5147b2dad4a5a098a23500029
Roberto Roxo <12330@ismt.pt>
2025-03-17 11:15:57 +0000

    //tsconfig delete, filtro resolvido Home

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,12 @@
 // Uncomment this line to use CSS modules
 import styles from './app.module.css';
+import Home from '../pages/Home';
 
 
 export function App() {
   return (
     <div>
-       <h1 title="Pokedex"></h1>
+       <Home />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { Grid, Container, CircularProgress } from "@mui/material";
-import { getPokemonList, getPokemonDetails } from "../api/pokemonApi";
-import { Pokemon, PokemonListItem } from "../types/pokemon";
+// import { Grid, Container, CircularProgress } from "@mui/material";
+// import { getPokemonList, getPokemonDetails } from "../api/pokemonApi";
+// import { Pokemon, PokemonListItem } from "../types/pokemon";
 import PokemonCard from "../components/PokeCard";
 import { staticPokemonList } from "../utils/data";
 
@@ -9,13 +9,13 @@ import { staticPokemonList } from "../utils/data";
 const Home = () => {
 
     //State para o valor da pesquisa
-    const[searchValue, setSearchValue] = useState<string>("");
+    const[searchValue, setSearchValue] = useState<string>(" ");
     //State para os resultados filtrados
     const[filteredPokemon, setFilteredPokemon] = useState(staticPokemonList);
 
     useEffect(() => {
         const filtered = staticPokemonList.filter((pokemon) =>
-            pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
+            pokemon.name.toLowerCase().includes(searchValue.toLowerCase().trim())
           );
           setFilteredPokemon(filtered);
         }, [searchValue]);
@@ -23,15 +23,18 @@ const Home = () => {
         const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
             setSearchValue(event.target.value);
           };
-    return (
-        <div>
-            {filteredPokemon.length > 0 ? (
-                filteredPokemon.map((pokemon) => (
-            <PokemonCard pokemon={pokemon} />
-            ) : (
-            <p>Nenhum Pokémon encontrado!</p>
-          )}
-        </div>
+          console.log(filteredPokemon);
+          return (
+            <div>
+                {filteredPokemon.length > 0 ? (
+                    filteredPokemon.map((pokemon) => (
+                        <PokemonCard key={pokemon.id} />
+                    ))
+                ) : (
+                    <p>Nenhum Pokémon encontrado!</p>
+                )}
+            </div>
+        );
 
 };
 export default Home;
